b2958ecf59f9c96714b41f2468f0577247098eeb
sleopard1179 <michael@stackonapp.com>
2021-01-23 14:08:23 -0500

    Putting the cards in to finish design next

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,14 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  template: `
+  <ion-header [translucent]="true">
+    <ion-toolbar>
+      <ion-buttons slot="end">
+        <img id="right-bar-icon" src="assets/nfc-link-icon/nfc-icon.png" (click)="nextPage()">
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-content [fullscreen]="true">
+    <ion-header collapse="condense">
+      <ion-toolbar>
+        <ion-title size="large">Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <div id="bg"></div>
+    <div id="container">
+      <strong>Ready to create an app?</strong>
+      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
+  </ion-content>
+  `
 })
 export class HomePage {
 
+  testVar = 0;
+
   constructor() {}
 
-  nextPage() {
+  public nextPage(): void {
+    this.testVar = 1;
     console.log('Testing Function');
   }
 
