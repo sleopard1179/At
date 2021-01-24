49d5e9c7515a71ad3d49bc8cf8a429545be70359
sleopard1179 <michael@stackonapp.com>
2021-01-24 09:19:47 -0500

    Safe commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,6 +18,7 @@ import { Component } from '@angular/core';
       <app-cards [isHomeScreen]="true"></app-cards>
       <!--<strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p> -->
+      <app-cards [isHomeScreen]="false"></app-cards>
     </div>
     <app-fabs></app-fabs>
   </ion-content>
