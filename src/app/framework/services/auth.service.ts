5b64ddcc3765c960feac8df29768930682d16a4f
sleopard1179 <michael@stackonapp.com>
2021-01-28 10:27:21 -0500

    Safe push

--- src/app/framework/services/auth.service.ts
+++ src/app/framework/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpRequest, HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
     providedIn: 'root'
