3e5bb9cf9487d5dc8783aeab947f293874ab77b5
sleopard1179 <michael@stackonapp.com>
2021-01-27 22:13:02 -0500

    Added Capacitor Support

--- src/app/framework/services/auth.service.ts
+++ src/app/framework/services/auth.service.ts
@@ -2,14 +2,12 @@ import { HttpRequest, HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
-
 @Injectable({
     providedIn: 'root'
 })
 
 export class AuthService {
 
-    cachedRequests = Array<HttpRequest<any>> = [];
-    rootApi = 'http://localhost:5000/api/auth';
-    rootApi =
+    
+    
 }
